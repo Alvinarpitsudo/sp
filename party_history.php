0876bd8ffe917444baa09c7b2cdfb3e1b8b9dccd
alvin <alvinarpit@zoyoecommerce.com>
2021-11-26 18:20:48 +0530

    party history

---
+++ party_history.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>पार्टी इतिहास - समाजवादी पार्टी, बिजनौर</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/logo.JPG" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include('header.php') ?>
+
+    <main id="main">
+
+        <!-- ======= Section ======= -->
+        <section id="development-initiative" class="development-initiative">
+            <div class="container-fluid dev-init">
+
+                <div class="section-title">
+                    <h2>पार्टी इतिहास</h2>
+                </div>
+            </div>
+
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="assets/img/ourhistory/2.jpg" class="d-block w-100 img-fluid" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h1 class="text-success fw-bold">1992</h1>
+                            <h3 class="text-dark fw-bold">First National Conference, 4th November, 1992</h3>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="assets/img/ourhistory/3.jpg" class="d-block w-100 img-fluid" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h1 class="text-success fw-bold">1993</h1>
+                            <h3 class="text-dark fw-bold">Shri. Mulayam Singh Yadav - Second terms as Chief Minister, Uttar Pradesh</h3>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="assets/img/ourhistory/4.jpg" class="d-block w-100 img-fluid" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h1 class="text-success fw-bold">1994</h1>
+                            <h3 class="text-dark fw-bold">Second National Conference , Uttar Pradesh, 1993</h3>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="assets/img/ourhistory/4.jpg" class="d-block w-100 img-fluid" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h1 class="text-success fw-bold">1993</h1>
+                            <h3 class="text-dark fw-bold">Shri. Mulayam Singh Yadav - Second terms as Chief Minister, Uttar Pradesh, 1993</h3>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="assets/img/ourhistory/4.jpg" class="d-block w-100 img-fluid" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h1 class="text-success fw-bold">1993</h1>
+                            <h3 class="text-dark fw-bold">Shri. Mulayam Singh Yadav - Second terms as Chief Minister, Uttar Pradesh, 1993</h3>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span><i class="bi bi-arrow-left-circle-fill icon"></i></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span><i class="bi bi-arrow-right-circle-fill icon"></i></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </section>
+    </main>
+    <!-- End #main -->
+
+    <?php include('footer.php') ?>
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- JS Files -->
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
