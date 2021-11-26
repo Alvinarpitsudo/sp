11a4676be73927b454959458c723ba9863f6a84e
alvin <alvinarpit@zoyoecommerce.com>
2021-11-26 18:19:52 +0530

    about party

---
+++ about_party.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>पार्टी के बारे में - समाजवादी पार्टी, बिजनौर</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/logo.JPG" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include('header.php') ?>
+
+    <main id="main">
+
+        <!-- ======= Section ======= -->
+        <section id="development-initiative" class="development-initiative">
+            <div class="container dev-init">
+
+                <div class="section-title">
+                    <h2>पार्टी के बारे में</h2>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4 mb-4">
+                        <div class="card">
+                            <img src="assets/img/aboutparty/1.jpg" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title"><a href="">पार्टी इतिहास</a></h5>
+                                <p class="card-text">समाजवादी पार्टी की स्थापना 4 अक्टूबर 1992 को मुलायम सिंह यादव ने की थी और यह उत्तर प्रदेश में स्थित है।...
+                                </p>
+                            </div>
+                            <a href="" class="btn-know-more">अधिक जानिए <i class="bi bi-caret-right-fill"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-4">
+                        <div class="card">
+                            <img src="assets/img/aboutparty/mulayam.jpg" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title"><a href="">पार्टी विचारधारा</a></h5>
+                                <p class="card-text">समाजवादी पार्टी समानता के सिद्धांत पर काम करने वाले समाजवादी समाज के निर्माण में विश्वास रखती है।...
+                                </p>
+                            </div>
+                            <a href="" class="btn-know-more">अधिक जानिए <i class="bi bi-caret-right-fill"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-4">
+                        <div class="card">
+                            <img src="assets/img/aboutparty/party-constitution.jpg" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title"><a href="">पार्टी संविधान</a></h5>
+                                <p class="card-text">समाजवादी पार्टी के संविधान को 2011 में आगरा, उत्तर प्रदेश में आठवें राष्ट्रीय सम्मेलन में संशोधित किया गया था।...
+                                </p>
+                            </div>
+                            <a href="" class="btn-know-more">अधिक जानिए <i class="bi bi-caret-right-fill"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6 mb-4">
+                        <div class="card">
+                            <img src="assets/img/aboutparty/1.jpg" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title"><a href="">हमारे नेता</a></h5>
+                                <p class="card-text">मुलायम सिंह यादव समाजवादी पार्टी के संस्थापक हैं और अखिलेश जनवरी 2017 से अध्यक्ष हैं।...
+                                </p>
+                            </div>
+                            <a href="" class="btn-know-more">अधिक जानिए <i class="bi bi-caret-right-fill"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 mb-4">
+                        <div class="card">
+                            <img src="assets/img/aboutparty/president.jpg" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title"><a href="">पार्टी अध्यक्ष</a></h5>
+                                <p class="card-text">माननीय श्री अखिलेश यादव का जन्म 1 जुलाई 1973 को मुलायम सिंह यादव और मालती देवी के सैफई में हुआ था।...
+                                </p>
+                            </div>
+                            <a href="" class="btn-know-more">अधिक जानिए <i class="bi bi-caret-right-fill"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Section -->
+    </main>
+    <!-- End #main -->
+
+    <?php include('footer.php') ?>
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- JS Files -->
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
