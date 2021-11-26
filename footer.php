a3827b6daa6ac097ca02fc70ae1a641391070ed1
alvin <alvinarpit@zoyoecommerce.com>
2021-11-26 18:20:03 +0530

    footer

---
+++ footer.php
@@ -0,0 +1,104 @@
+<!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-newsletter">
+      <div class="container py-3">
+        <div class="row my-3 text-center">
+          <div class="col-md-6 mb-4" data-aos="fade-up">
+            <h4>फेसबुक</h4>
+            <aside class="widget" style="width: auto;">
+              <div class="fb-page"
+                data-href="https://www.facebook.com/pages/category/Political-Party/Samajwadi-Party-Bijnor-106739211205515/"
+                data-tabs="timeline" data-width="480" data-height="450" data-small-header="false"
+                data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+                <div
+                  cite="https://www.facebook.com/pages/category/Political-Party/Samajwadi-Party-Bijnor-106739211205515/"
+                  class="fb-xfbml-parse-ignore"><a
+                    href="https://www.facebook.com/pages/category/Political-Party/Samajwadi-Party-Bijnor-106739211205515/">Facebook</a>
+                </div>
+              </div>
+              <div class="textwidget custom-html-widget">
+                <script async src="https://connect.facebook.net/hi_IN/sdk.js#xfbml=1&version=v11.0"
+                  nonce="gMaj3cox"></script>
+              </div>
+            </aside>
+          </div>
+          <div class="col-md-6 mb-4" data-aos="fade-up">
+            <h4>ट्विटर</h4>
+            <aside id="custom_html-3" style="width: auto;" class="widget_text widget widget_custom_html">
+              <div class="textwidget custom-html-widget"><a class="twitter-timeline" data-width="480" data-height="450"
+                  href="https://twitter.com/samajwadibijnor">Tweets by BJP4Bjinor</a>
+                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+              </div>
+            </aside>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-top">
+      <div class="container-fluid">
+        <div class="row justify-content-center">
+          <div class="col-xl-10">
+            <div class="row">
+
+              <div class="col-lg-3 col-md-6 footer-links">
+                <h4>उपयोगी कड़ियां</h4>
+                <ul>
+                  <li><i class="bx bx-chevron-right"></i> <a href="#">होम</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="#">पार्टी के बारे में</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="#">जिला अध्यक्ष</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="#">सेवा की शर्तें</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="#">गोपनीयता नीति</a></li>
+                </ul>
+              </div>
+
+              <div class="col-lg-3 col-md-6 footer-links">
+                <h4>अन्य लिंक</h4>
+                <ul>
+                  <li><i class="bx bx-chevron-right"></i> <a href="#">राज्य सभा के सदस्य</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="#">लोकसभा के सदस्य</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="#">विधान सभा के सदस्य</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="#">समाजवादी बुलेटिन</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="#">सरकारी प्रवक्ता</a></li>
+                </ul>
+              </div>
+
+              <div class="col-lg-3 col-md-6 footer-contact">
+                <h4>संपर्क करें</h4>
+                <p>
+                  समाजवादी पार्टी बिजनौर - 246701
+                  <br>
+                  उत्तर प्रदेश, भारत
+                  <br>
+                  <br>
+                  <strong>फोन:</strong> +91 0000 0000 000<br>
+                  <strong>ईमेल:</strong> info@example.com<br>
+                </p>
+
+              </div>
+
+              <div class="col-lg-3 col-md-6 footer-info">
+                <h3>सोशल मीडिया लिंक</h3>
+                <div class="social-links mt-3">
+                  <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                  <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                  <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; 2021 Copyright <strong><span>समाजवादी पार्टी बिजनौर</span></strong>, All Rights Reserved.
+      </div>
+      <div class="credits">
+        Developed by: <a href="https://zoyoecommerce.com/">Zoyo E-Commerce Pvt. Ltd.</a>
+      </div>
+    </div>
+  </footer>
+  <!-- End Footer -->
