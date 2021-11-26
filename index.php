91ef19478f01aeefdd6ecb35c7cf915119abdf2a
alvin <alvinarpit@zoyoecommerce.com>
2021-11-26 18:20:28 +0530

    index

---
+++ index.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>समाजवादी पार्टी, बिजनौर</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/logo.JPG" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+  <?php include('header.php') ?>
+
+  <!-- header carousel -->
+  <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="assets/img/slide/1.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="assets/img/slide/2.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="assets/img/slide/1.jpg" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span><i class="bi bi-arrow-left-circle-fill icon"></i></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span><i class="bi bi-arrow-right-circle-fill icon"></i></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+  <!-- End carousel -->
+
+  <main id="main">
+
+    <section id="video-button" class="video-button section-bg">
+      <div class="container">
+        <div class="row text-center justify-content-center">
+          <div class="col-lg-3 mt-5">
+            <a href="" class="v-btn"><i class="bi bi-film"></i> प्रेस प्रकाशनी</a>
+          </div>
+          <div class="col-lg-3 mt-5">
+            <a href="" class="v-btn"><i class="bi bi-film"></i> लाइव डेमो</a>
+          </div>
+          <div class="col-lg-3 mt-5">
+            <a href="" class="v-btn"><i class="bi bi-film"></i> विजन दस्तावेज़ 2019</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2 class="mb-4">हमारी विचारधारा</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-md-4">
+            <p class="para mt-4">
+              एक धर्मनिरपेक्ष और लोकतांत्रिक विचारधारा के साथ समाजवादी पार्टी एक समाजवादी समाज बनाने में विश्वास करती
+              है,
+              जो समानता के सिद्धांत पर काम करता है। समाजवादी पार्टी इस दिशा में लगातार काम करने में विश्वास रखती है
+              समाज के कमजोर वर्गों का उत्थान और यह सांप्रदायिक ताकतों के खिलाफ मजबूती से खड़ा है।
+            </p>
+          </div>
+
+          <div class="col-md-8 icon-boxes">
+            <div class="row">
+              <div class="col-md-4">
+                <div class="icon-box align-items-center text-center">
+                  <img src="assets/img/democratic.png" class="img-fluid" alt="">
+                  <h3>लोकतांत्रिक</h3>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="icon-box align-items-center text-center">
+                  <img src="assets/img/socialist.png" class="img-fluid" alt="">
+                  <h3 class="socialist">समाजवादी</h3>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="icon-box align-items-center text-center">
+                  <img src="assets/img/secular.png" class="img-fluid" alt="">
+                  <h3>धर्मनिरपेक्ष</h3>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-4">
+                <div class="icon-box align-items-center text-center">
+                  <img src="assets/img/equality.png" class="img-fluid" alt="">
+                  <h3>समानता</h3>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="icon-box align-items-center text-center">
+                  <img src="assets/img/integrity.png" class="img-fluid" alt="">
+                  <h3>अखंडता</h3>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="icon-box align-items-center text-center">
+                  <img src="assets/img/progress.png" class="img-fluid" alt="">
+                  <h3>प्रगति</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <!-- End About Section -->
+
+    <!-- ======= Section ======= -->
+    <section id="development-initiative" class="development-initiative">
+      <div class="container dev-init">
+
+        <div class="section-title">
+          <h2>विकास पहल</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-md-4 mb-4">
+            <div class="card">
+              <img src="assets/img/dev_init/ini-business.jpg" class="card-img-top" alt="">
+              <div class="card-body">
+                <h5 class="card-title"><a href="">व्यापार पहल</a></h5>
+                <p class="card-text">उत्तर प्रदेश में उद्यमी अब सरकार समर्थित कई योजनाओं का लाभ उठा सकते हैं।
+                  समाजवादी पार्टी उत्तर प्रदेश को घर के रूप में चुनने के लिए उद्यमियों को प्रोत्साहित करती है...
+                </p>
+              </div>
+              <a href="" class="btn-know-more">अधिक जानिए <i class="bi bi-caret-right-fill"></i></a>
+            </div>
+          </div>
+          <div class="col-md-4 mb-4">
+            <div class="card">
+              <img src="assets/img/dev_init/ini-education.jpg" class="card-img-top" alt="">
+              <div class="card-body">
+                <h5 class="card-title"><a href="">शिक्षा पहल</a></h5>
+                <p class="card-text">यूनिसेफ इंडिया एनुअल स्टेटस ऑफ एजुकेशन रिपोर्ट, 2015 के अनुसार, "उत्तर"
+                  भारत के सबसे अधिक आबादी वाले राज्य प्रदेश (यूपी) ने प्राइमरी में प्रभावशाली बढ़त हासिल की है...
+                </p>
+              </div>
+              <a href="" class="btn-know-more">अधिक जानिए <i class="bi bi-caret-right-fill"></i></a>
+            </div>
+          </div>
+          <div class="col-md-4 mb-4">
+            <div class="card">
+              <img src="assets/img/dev_init/ini-industry.jpg" class="card-img-top" alt="">
+              <div class="card-body">
+                <h5 class="card-title"><a href="">औद्योगिक पहल</a></h5>
+                <p class="card-text">कृषि तकनीकों के आधुनिकीकरण के लिए औद्योगिक विकास महत्वपूर्ण,
+                  विज्ञान और प्रौद्योगिकी, नई पूंजी निर्माण को नियोजित करके नए नवाचारों का विकास करना....
+                </p>
+              </div>
+              <a href="" class="btn-know-more">अधिक जानिए <i class="bi bi-caret-right-fill"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-4 mb-4">
+            <div class="card">
+              <img src="assets/img/dev_init/ini-infra.jpg" class="card-img-top" alt="">
+              <div class="card-body">
+                <h5 class="card-title"><a href="">बुनियादी ढांचा पहल</a></h5>
+                <p class="card-text">समाजवादी पार्टी वितरण, रखरखाव, और में सुधार पर केंद्रित है
+                  भौतिक बुनियादी ढांचे का संचालन। पार्टी ने इसे विभिन्न माध्यमों से पूरा किया है...
+                </p>
+              </div>
+              <a href="" class="btn-know-more">अधिक जानिए <i class="bi bi-caret-right-fill"></i></a>
+            </div>
+          </div>
+          <div class="col-md-4 mb-4">
+            <div class="card">
+              <img src="assets/img/dev_init/ini-education.jpg" class="card-img-top" alt="">
+              <div class="card-body">
+                <h5 class="card-title"><a href="">महिला सशक्तिकरण</a></h5>
+                <p class="card-text">समाजवादी पार्टी ने हमेशा महिला सशक्तिकरण को अत्यधिक महत्व दिया है और
+                  उनकी शिक्षा आर्थिक स्वतंत्रता के लिए कई योजनाएं शुरू कीं...
+                </p>
+              </div>
+              <a href="" class="btn-know-more">अधिक जानिए <i class="bi bi-caret-right-fill"></i></a>
+            </div>
+          </div>
+          <div class="col-md-4 mb-4">
+            <div class="card">
+              <img src="assets/img/dev_init/ini-industry.jpg" class="card-img-top" alt="">
+              <div class="card-body mb-4">
+                <h5 class="card-title"><a href="">कृषि पहल</a></h5>
+                <p class="card-text">समाजवादी पार्टी ने हमेशा महिला सशक्तिकरण को अत्यधिक महत्व दिया है और
+                  है......
+                </p>
+              </div>
+              <a href="" class="btn-know-more">अधिक जानिए <i class="bi bi-caret-right-fill"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End Section -->
+
+    <section id="president" class="president">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>पार्टी अध्यक्ष</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-md-4">
+            <img src="assets/img/president.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-md-8 akhilesh-about">
+            <h3>श्री अखिलेश यादव</h3>
+            <span>अध्यक्ष &middot; समाजवादी पार्टी</span>
+            <p>
+              1 जुलाई 1973 को सैफई में मुलायम सिंह यादव और मालती देवी के यहां जन्मे अखिलेश यादव ने प्राप्त की
+              राजस्थान के धौलपुर मिलिट्री स्कूल में प्रारंभिक शिक्षा.
+            </p>
+            <a href="" class="btn-know-more">अधिक जानिए <i class="bi bi-caret-right-fill"></i></a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact section-bg">
+      <div class="container-fluid">
+
+        <div class="section-title">
+          <h2>संपर्क</h2>
+          <h3>हमारे साथ संपर्क में <span>रहें</span></h3>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-xl-10">
+            <div class="row">
+
+              <div class="col-lg-6">
+
+                <div class="row justify-content-center">
+
+                  <div class="col-md-12 info d-flex flex-column align-items-stretch">
+                    <i class="bx bx-map"></i>
+                    <h4>पता</h4>
+                    <p>समाजवादी पार्टी बिजनौर - 246701,<br>उत्तर प्रदेश, भारत</p>
+                  </div>
+                  <div class="col-md-6 info d-flex flex-column align-items-stretch">
+                    <i class="bx bx-phone"></i>
+                    <h4>कॉल करें</h4>
+                    <p>+91 0000 0000 00<br>+91 0000 0000 00</p>
+                  </div>
+                  <div class="col-md-6 info d-flex flex-column align-items-stretch">
+                    <i class="bx bx-envelope"></i>
+                    <h4>ईमेल</h4>
+                    <p>contact@example.com<br>info@example.com</p>
+                  </div>
+                </div>
+
+              </div>
+
+              <div class="col-lg-6">
+                <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                  <div class="row">
+                    <div class="col-md-6 form-group">
+                      <label for="name">आपका नाम</label>
+                      <input type="text" name="name" class="form-control" id="name" required>
+                    </div>
+                    <div class="col-md-6 form-group mt-3 mt-md-0">
+                      <label for="email">आपका ईमेल</label>
+                      <input type="email" class="form-control" name="email" id="email" required>
+                    </div>
+                  </div>
+                  <div class="form-group mt-3">
+                    <label for="subject">विषय</label>
+                    <input type="text" class="form-control" name="subject" id="subject" required>
+                  </div>
+                  <div class="form-group mt-3">
+                    <label for="message">संदेश</label>
+                    <textarea class="form-control" name="message" rows="8" required></textarea>
+                  </div>
+                  <div class="my-3">
+                    <div class="loading">Loading</div>
+                    <div class="error-message"></div>
+                    <div class="sent-message">Your message has been sent. Thank you!</div>
+                  </div>
+                  <div class="text-center"><button type="submit">संदेश भेजें</button></div>
+                </form>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <!-- End Contact Section -->
+  </main>
+  <!-- End #main -->
+
+  <?php include('footer.php') ?>
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- JS Files -->
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+  <!-- Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
