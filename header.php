b7a71897bfd62652981d1420071245a3deeb7491
alvin <alvinarpit@zoyoecommerce.com>
2021-11-26 18:20:15 +0530

    header

---
+++ header.php
@@ -0,0 +1,76 @@
+ <!-- ======= Top Bar ======= -->
+ <section id="topbar" class="d-flex align-items-center">
+   <div class="container d-flex justify-content-center justify-content-md-between">
+     <div class="contact-info d-flex align-items-center">
+       <i class="bi bi-envelope d-flex align-items-center d-none d-md-block"><a href="mailto:contact@example.com">contact@example.com</a></i>
+       <i class="bi bi-phone d-flex align-items-center ms-4 d-none d-md-block"><span>+91 0000 0000 00</span></i>
+     </div>
+     <div class="social-links d-md-flex align-items-center">
+       <a href="https://twitter.com/samajwadibijnor" class="twitter"><i class="bi bi-twitter"></i></a>
+       <a href="https://www.facebook.com/pages/category/Political-Party/Samajwadi-Party-Bijnor-106739211205515" class="facebook"><i class="bi bi-facebook"></i></a>
+     </div>
+   </div>
+ </section>
+
+ <!-- ======= Header ======= -->
+ <header id="header" class="sticky-top header-inner-pages">
+   <div class="container d-flex align-items-center justify-content-between">
+
+     <!-- Uncomment below if you prefer to use an image logo -->
+     <a href="index.html" class="logo"><img src="assets/img/logo.JPG" alt="" class="img-fluid"></a>
+
+     <nav id="navbar" class="navbar">
+       <ul>
+         <li><a class="nav-link scrollto active" href="./">होम</a></li>
+         <li class="dropdown"><a href="#"><span>बारे में</span> <i class="bi bi-chevron-down"></i></a>
+           <ul>
+             <li><a href="about_party.php">पार्टी के बारे में</a></li>
+             <li><a href="party_history.php">पार्टी का इतिहास</a></li>
+             <li><a href="#">पार्टी विचारधारा</a></li>
+             <li><a href="#">पार्टी संविधान</a></li>
+             <li><a href="#">पार्टी नेता</a></li>
+             <li><a href="#">पार्टी अध्यक्ष</a></li>
+           </ul>
+         </li>
+         <li><a class="nav-link scrollto" href="#portfolio">जिला अध्यक्ष</a></li>
+         <li class="dropdown"><a href="#"><span>सदस्य</span> <i class="bi bi-chevron-down"></i></a>
+           <ul>
+             <li><a href="#">विधान परिषद के सदस्य</a></li>
+             <li><a href="#">राज्यसभा के सदस्य</a></li>
+             <li><a href="#">लोकसभा के सदस्य</a></li>
+             <li><a href="#">विधान सभा के सदस्य</a></li>
+           </ul>
+         </li>
+         <li class="dropdown"><a href="#"><span>विकास पहल</span> <i class="bi bi-chevron-down"></i></a>
+           <ul>
+             <li><a href="#">महिला सशक्तिकरण की पहल</a></li>
+             <li><a href="#">बुनियादी ढांचा पहल</a></li>
+             <li><a href="#">कृषि पहल</a></li>
+             <li><a href="#">व्यापार पहल</a></li>
+             <li><a href="#">औद्योगिक पहल</a></li>
+             <li><a href="#">शिक्षा पहल</a></li>
+           </ul>
+         </li>
+         <li class="dropdown"><a href="#"><span>मीडिया</span> <i class="bi bi-chevron-down"></i></a>
+           <ul>
+             <li><a href="#">प्रेस विज्ञप्ति</a></li>
+             <li><a href="#">समाजवादी बुलेटिन</a></li>
+             <li><a href="#">समाचार</a></li>
+             <li><a href="#">फोटो गैलरी</a></li>
+             <li><a href="#">वीडियो गैलरी</a></li>
+             <li><a href="#">सरकारी प्रवक्ता</a></li>
+           </ul>
+         </li>
+         <li class="dropdown"><a href="#"><span>पार्टी में शामिल हों</span> <i class="bi bi-chevron-down"></i></a>
+           <ul>
+             <li><a href="#">सदस्य बनें</a></li>
+           </ul>
+         </li>
+         <li><a class="nav-link scrollto" href="#contact">संपर्क</a></li>
+       </ul>
+       <i class="bi bi-list mobile-nav-toggle"></i>
+     </nav>
+     <!-- .navbar -->
+   </div>
+ </header>
+ <!-- End Header -->
