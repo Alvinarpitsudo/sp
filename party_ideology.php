c2a6ca980a9d56ee000dd954bf8f2e85fc55307b
alvin <alvinarpit@zoyoecommerce.com>
2021-11-27 18:23:10 +0530

    party ideology

---
+++ party_ideology.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>पार्टी विचारधारा - समाजवादी पार्टी, बिजनौर</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/logo.JPG" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include('header.php') ?>
+
+    <main id="main">
+
+        <section id="president" class="president">
+            <div class="container">
+
+                <div class="section-title">
+                    <h2>पार्टी विचारधारा</h2>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-5 d-flex align-items-center justify-content-center">
+                        <img src="assets/img/Mulayam_Singh_Yadav.jpg" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-md-7 akhilesh-about">
+                        <h3>श्री मुलायम सिंह यादव</h3>
+                        <span>समाजवादी पार्टी</span>
+                        <p>
+                            धर्मनिरपेक्ष और लोकतांत्रिक विचारधारा के साथ समाजवादी पार्टी एक समाजवादी समाज बनाने में विश्वास रखती है, जो समानता के सिद्धांत पर काम करता है। समाजवादी पार्टी समाज के कमजोर वर्गों के उत्थान की दिशा में लगातार काम करने में विश्वास रखती है और सांप्रदायिक ताकतों के खिलाफ मजबूती से खड़ी है।
+                        </p>
+                        <p>
+                            स्वतंत्रता सेनानी, समाजवादी और महान सांसद श्री राम मनोहर लोहिया समाजवादी पार्टी के पथ प्रदर्शक हैं। श्री राम मनोहर लोहिया की अखंडता, भारत की स्वतंत्रता के लिए निस्वार्थ संघर्ष और समाज के सभी वर्गों के लोगों को एकजुट करने की उनकी क्षमता ने पार्टी के नेताओं, युवाओं और कार्यकर्ताओं को बहुत प्रभावित किया है। लोहिया स्वतंत्रता संग्राम के दौरान कई बार अंग्रेजों द्वारा जेल गए और उन्होंने सामाजिक समानता के लिए जीवन भर कड़ा संघर्ष किया।
+                        </p>
+                        <p>
+                            महात्मा गांधी के विचारों ने लोहिया को बहुत प्रभावित किया जिन्होंने स्वतंत्रता आंदोलन के दौरान अंग्रेजों के खिलाफ लेख लिखने, यूरोप में ब्रिटिश साम्राज्यवाद के खिलाफ जागरूकता बढ़ाने के लिए अथक प्रयास किया। भारत के स्वतंत्र होने के बाद, उन्होंने खुद को जमीनी स्तर की राजनीति के लिए समर्पित कर दिया जिसमें किसान हित, विकास कार्य और सामाजिक अन्याय के खिलाफ लड़ाई शामिल थी। उन्होंने पूंजीवादी-सामंतवादी प्रवृत्तियों को समाप्त करने की दिशा में भी काम किया।
+                        </p>
+                        <p>
+                            सत्याग्रह पर अपने ज्वलंत लेखों और सामाजिक समस्याओं के अपने सहज ज्ञान से लोगों को जगाने की लोहिया की क्षमता ने उन्हें भारत का अग्रणी समाजवादी नेता बना दिया। लोहिया की 'करो या मरो' की भावना समाज के गरीब, पिछड़े और सामाजिक रूप से कमजोर वर्गों के सामाजिक उत्थान के संघर्ष में समाजवादी पार्टी के लिए एक प्रेरणा और मार्गदर्शक शक्ति है।
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <!-- End #main -->
+
+    <?php include('footer.php') ?>
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- JS Files -->
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
