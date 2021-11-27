a1f50699419f3273deb4202690fe3a00836a12e8
alvin <alvinarpit@zoyoecommerce.com>
2021-11-27 18:23:27 +0530

    party leader

---
+++ party_leader.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>पार्टी नेता - समाजवादी पार्टी, बिजनौर</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/logo.JPG" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include('header.php') ?>
+
+    <main id="main">
+
+        <section id="president" class="president">
+            <div class="container">
+
+                <div class="section-title">
+                    <h2>पार्टी नेता</h2>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-5 d-flex align-items-center justify-content-center">
+                        <img src="assets/img/aboutparty/1.jpg" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-md-7 akhilesh-about">
+                        <h3>श्री मुलायम सिंह यादव</h3>
+                        <span>समाजवादी पार्टी</span>
+                        <p>
+                            श्री मुलायम सिंह यादव जी का जन्म 22 नवंबर 1939 को इटावा में हुआ था। प्यार से और श्रद्धा से नेताजी के रूप में संबोधित किए जाने पर, वे श्री राम मनोहर लोहिया से बहुत प्रभावित हुए और 1950 के दशक के दौरान किसानों के अधिकारों के लिए लड़ने के लिए महान समाजवादी नेता के आंदोलन में शामिल हो गए। उन्हें भारत के पूर्व प्रधान मंत्री चौधरी चरण सिंह द्वारा 'लिटिल नेपोलियन' कहा जाता था।
+                        </p>
+                        <p>
+                            वे पहली बार 1967 में यूपी विधानसभा में विधायक बने और 1975 में आपातकाल के दौरान जेल गए। फिर वह समय आया जब अक्टूबर 1992 में समाजवादी पार्टी अस्तित्व में आई। पार्टी भारत के महान नेता श्री राम के समाजवादी सिद्धांतों से प्रेरित थी। मनोहर लोहिया. नेताजी ने 1989 से 1991 तक, 1993 से 1995 तक और 2003 से 2007 तक उत्तर प्रदेश के मुख्यमंत्री के रूप में कार्य किया है।
+                        </p>
+                        <p>
+                            उन्होंने नीतियों को लागू किया और यह सुनिश्चित करने के लिए कार्रवाई की कि समाज के गरीब और पिछड़े वर्ग सद्भाव में रहें, और सभी धर्मों, जातियों और सामाजिक पृष्ठभूमि के लोगों ने राज्य के विकास में योगदान दिया। उन्होंने संयुक्त मोर्चा सरकार में 1996 से 1998 तक भारत सरकार में रक्षा मंत्री के रूप में भी कार्य किया। नेताजी के नेतृत्व में समाजवादी पार्टी गांवों और कस्बों में समाज के विभिन्न वर्गों के लोगों के लिए एक छत्र बन गई है। नेताजी ने समाजवादी पार्टी को समाज के दलित और उपेक्षित वर्गों के लिए एक वाहन बनाने का प्रयास किया है। उनके महत्वपूर्ण और निर्णायक नेतृत्व में, पार्टी ने शानदार शर्तें देखी हैं और वह पार्टी के सभी सदस्यों को अपनी मातृभूमि की बेहतरी के लिए कड़ी मेहनत करने और कड़ी मेहनत करने के लिए प्रेरित करना जारी रखते हैं। विश्वसनीय रूप से लागत प्रभावी प्लेटफार्मों को समय-समय पर साझेदारी में संलग्न करें। स्केलेबल ई-बिजनेस के दौरान फॉस्फोरसेंटली एंड-टू-एंड सर्वोत्तम प्रथाओं का पुनर्पूंजीकरण। के बजाय ऊर्जावान रूप से 2.0 मानव पूंजी को ई-सक्षम करें।
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <!-- End #main -->
+
+    <?php include('footer.php') ?>
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- JS Files -->
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
